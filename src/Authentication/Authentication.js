f4b9423f129b053222f94cf4d041b6cdc5404bca
ByteZO <coder.gny@gmail.com>
2024-04-26 08:52:45 +0530

    #3 Auth , Database and Storage function ...

---
+++ src/Authentication/Authentication.js
@@ -0,0 +1,48 @@
+import { Client, Account, ID } from "appwrite";
+import Config from "../Config/Config";
+
+class Auth {
+  client = new Client();
+  account;
+  constructor() {
+    this.client.setEndpoint(Config.appWriteUrl).setProject(Config.appWriteId);
+    this.account = new Account(this.client);
+  }
+
+  async createAccount({ email, password, name }) {
+    try {
+      const newAccount = await this.account.create(
+        ID.unique,
+        email,
+        password,
+        name
+      );
+
+      if (newAccount) {
+        //login
+        return await this.logIn(email, password);
+      } else {
+        return newAccount;
+      }
+    } catch (error) {
+      throw error;
+    }
+  }
+  async getCurrentUser() {
+    try {
+      return await this.account.get();
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async logIn({ email, password }) {
+    return await this.account.createEmailSession(email, password);
+  }
+  async logOut() {
+    return await this.account.deleteSessions();
+  }
+}
+
+const Authentication = new Auth();
+export default Authentication;
---
+++ src/Authentication/Configuration.js
@@ -0,0 +1,132 @@
+import { Client, Account, ID, Storage, Databases, Query } from "appwrite";
+import Config from "../Config/Config";
+
+class Services {
+  client = new Client();
+  Databases;
+  Bucket;
+
+  constructor() {
+    this.client.setEndpoint(Config.appWriteUrl).setProject(Config.appWriteId);
+    this.Databases = new Databases(this.client);
+    this.Bucket = new Storage(this.client);
+  }
+
+  async createPost({ title, slug, content, status, featuredImage, userId }) {
+    try {
+      return await this.Databases.createDocument(
+        Config.appWriteDatabase,
+        Config.appWriteCollection,
+        ID.unique(),
+        { title, slug, content, status, featuredImage, userId }
+      );
+    } catch (error) {
+      console.log("AppWrite Create Post Error :", error);
+    }
+  }
+
+  async updatePost(slug, { title, content, status, featuredImage }) {
+    try {
+      return await this.Databases.updateDocument(
+        Config.appWriteDatabase,
+        Config.appWriteCollection,
+        slug,
+        {
+          title,
+          status,
+          content,
+          featuredImage,
+        }
+      );
+    } catch (error) {
+      console.log("AppWrite Update Post Error : ", error);
+    }
+  }
+
+  async deletePost(slug) {
+    try {
+      return await this.Databases.deleteDocument(
+        Config.appWriteDatabase,
+        Config.appWriteCollection,
+        slug
+      );
+    } catch (error) {
+      console.log("AppWrite Delete Post Error : ", error);
+    }
+  }
+
+  async getPost(slug) {
+    try {
+      return await this.Databases.getDocument(
+        Config.appWriteDatabase,
+        Config.appWriteCollection,
+        slug
+      );
+    } catch (error) {
+      console.log("AppWrite get Post Error : ", error);
+      return false;
+    }
+  }
+
+  async getPosts(query = [Query.equal("status", "active")]) {
+    try {
+      return await this.Databases.listDocuments(
+        Config.appWriteDatabase,
+        Config.appWriteCollection,
+        query
+      );
+    } catch (error) {
+      console.log("AppWrite get Posts Error : ", error);
+    }
+  }
+
+  //   File Handlling Methods
+
+  async uploadeImage(file) {
+    try {
+      return await this.Bucket.createFile(
+        Config.appWriteBucketId,
+        ID.unique(),
+        file
+      );
+    } catch (error) {
+      console.log("AppWrite uploade Image Error : ", error);
+      return false;
+    }
+  }
+
+  async deleteFile(fileId) {
+    try {
+      return await this.Bucket.deleteFile(Config.appWriteCollection, fileId);
+    } catch (error) {
+      console.log("AppWrite file delete Error : ", error);
+      return false;
+    }
+  }
+
+  async updateImage(fileId, file) {
+    try {
+      return await this.Bucket.updateFile(
+        Config.appWriteBucketId,
+        fileId,
+        file
+      );
+    } catch (error) {
+      console.log("AppWrite update Image Error : ", error);
+      return false;
+    }
+  }
+
+  async getFilePreview(fileId) {
+    try {
+      return await this.Bucket.getFilePreview(Config.appWriteBucketId, fileId);
+    } catch (error) {
+      console.log("AppWrite Image Preview Image Error : ", error);
+      return false;
+    }
+  }
+}
+
+const services = Services();
+
+export default services;
