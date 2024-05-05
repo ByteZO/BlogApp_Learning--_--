d7c783ff51095836f1776bd4404da89ba1f39f96
ByteZO <coder.gny@gmail.com>
2024-05-05 20:42:43 +0530

    Add postcss and tailwindcss configuration files

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -3,19 +3,20 @@ import Authentication from "../../Authentication/Authentication";
 import { useDispatch } from "react-redux";
 import { logIn, logOut } from "../../Store/AuthSlice";
 
-const [Loading, setLoading] = useState(true);
-const Dispatch = useDispatch();
-
-useEffect(() => {
-  Authentication.getCurrentUser()
-    .then((useData) => {
-      useData ? Dispatch(logIn(useData)) : Dispatch(logOut());
-    })
-    .finally(() => setLoading(false));
-});
 
 const Header = () => {
-  return !Loading ? <div> You are not LogIn !!! </div> : null;
+  const [Loading, setLoading] = useState(true);
+  const Dispatch = useDispatch();
+  useEffect(() => {
+    Authentication.getCurrentUser()
+      .then((useData) => {
+        useData ? Dispatch(logIn(useData)) : Dispatch(logOut());
+      })
+      .finally(() => setLoading(false));
+  });
+  return !Loading ? (
+    <div className=" min-h-screen bg-slate-600"> You are not LogIn !!! </div>
+  ) : null;
 };
 
 export default Header;
