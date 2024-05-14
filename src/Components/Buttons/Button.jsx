15605e0ff1f9287c68e98dd27635cd9930059b3f
ByteZO <coder.gny@gmail.com>
2024-05-14 20:05:08 +0530

    chore: Refactor Button component and add props for customization

--- src/Components/Buttons/Button.jsx
+++ src/Components/Buttons/Button.jsx
@@ -3,18 +3,29 @@ import { useDispatch } from "react-redux";
 import Authentication from "../../Authentication/Authentication";
 import { logOut } from "../../Store/AuthSlice";
 
-const Button = ({children}) => {
+const Button = ({
+  children,
+  type = "button",
+  bgColor = "bg-blue-600",
+  textColor = "text-white",
+  className = "",
+  ...props
+}) => {
   const dispatch = useDispatch();
   const logOutHandler = () => {
     Authentication.logOut().then(() => dispatch(logOut()));
   };
   return (
-    <div
-      className=" inline-block  px-6 py-2 duration-200 hover:bg-blue-100 rounded-full"
+    <button
+      className={`inline-block  px-6 py-2 duration-200 hover:bg-blue-100 rounded-full ${
+        (className, bgColor, textColor)
+      }`}
       onClick={logOutHandler}
+      typeof={type}
+      {...props}
     >
       {children}
-    </div>
+    </button>
   );
 };
 
